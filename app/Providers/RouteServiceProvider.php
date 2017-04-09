f1e56b7989b0cab75a11f86274c438c67943a92a
hasan hasibul <hasib32@gmail.com>
2017-04-09 10:35:28 -0400

    added users route and controller

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = 'App\Http\Controllers';
+    protected $namespace = 'App\Http\Controllers\V1';
 
     /**
      * Define your route model bindings, pattern filters, etc.
