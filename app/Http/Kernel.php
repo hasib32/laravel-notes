744e0dd815b01f2b06b683bc13933f9cf02c23c7
hasan hasibul <hasib32@gmail.com>
2017-04-09 20:53:22 -0400

    added CORS handler

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\VerifyApiVersion::class
+        \App\Http\Middleware\VerifyApiVersion::class,
+        \App\Http\Middleware\ModifyHeader::class,
     ];
 
     /**
---
+++ app/Http/Middleware/ModifyHeader.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ModifyHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //before request middleware
+        if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+            $data = json_decode($request->getContent(), true);
+            $request->request->replace(is_array($data) ? $data : array());
+        }
+
+        $response = $next($request);
+
+        //after request request middleware
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        
+        if($request->getMethod() === 'OPTIONS') {
+            $response->setStatusCode(204);
+        }
+
+        return $response;
+    }
+}
